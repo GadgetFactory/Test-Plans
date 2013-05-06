e695fd660b79f1e855923d5dc1dd3734d1a6d95f
Jack Gassett <jack.gassett@gadgetfactory.net>
2013-05-06 10:34:45 -0600

    LogicStart MegaWing Verification 1.0

--- LogicStart_MegaWing_Verification/LogicStart_MegaWing_Verification.ino
+++ LogicStart_MegaWing_Verification/LogicStart_MegaWing_Verification.ino
@@ -1,401 +1,270 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-#include "VGA.h"
-#include "SmallFS.h"
-#include "structures.h"
+#include "LogicStart.h"
+#include <SmallFS.h>
+#include "VGAZX.h"
+#include "fixedpoint.h"
 #include "cbuffer.h"
+#include "ptplay.h"
 
-// AUDIO STUFF
+#define USPIDATA16 *((&USPIDATA)+2)
 
-#define YM2149BASE IO_SLOT(11)
-#define YM2149REG(x) REGISTER(YM2149BASE,x)
+static SmallFSFile modfile;
+static char buf[128];
+static pt_mod_s * mod;
+static CircularBuffer<unsigned,7> audioBuffer;
 
-#define HAVE_YM
+extern pt_mod_s *pt_init_smallfs(SmallFSFile &file);
 
-struct ymframe {
-	unsigned char regval[16];
-};
-struct pokeyframe {
-	unsigned char regval[9];
-};
-CircularBuffer<ymframe,2> YMaudioBuffer;
-CircularBuffer<pokeyframe,2> POKEYaudioBuffer;
-
-SmallFSFile ymaudiofile;
-SmallFSFile pokeyaudiofile;
-
-#define AUDIOPIN WING_B_10
-
-#define BUTTON_DOWN  WING_B_13
-#define BUTTON_UP WING_B_14
-#define BUTTON_LEFT WING_B_12
-#define BUTTON_RIGHT WING_B_11
-#define BUTTON_RES WING_B_15 /* Reset */
-
-//For SPI ADC
-#define SELPIN WING_A_12 //Selection Pin
-#define DATAOUT WING_A_14//MOSI
-#define DATAIN  WING_A_13//MISO
-#define SPICLOCK WING_A_15 //Clock
-
-byte segmentPins[] = {WING_A_7, WING_A_10, WING_A_5, WING_A_6, WING_A_3, WING_A_4, WING_A_9}; // seg a to g
-
-byte digitPins[] = {WING_A_0,WING_A_2,WING_A_8,WING_A_11}; // LSB to MSB
-
-int ledPins[] = { 
-   WING_C_8,WING_C_9,WING_C_10,WING_C_11,WING_C_12,WING_C_13,WING_C_14,WING_C_15 };       // an array of pin numbers to which LEDs are attached
-int ledCount = 8;           // the number of pins (i.e. the length of the array)
-
-int switchPins[] = { 
-   WING_C_0,WING_C_1,WING_C_2,WING_C_3,WING_C_4,WING_C_5,WING_C_6,WING_C_7};       // an array of pin numbers to which Buttons are attached
-int switchCount = 8;           // the number of pins (i.e. the length of the array)
-
-char adc;
-int val;
-char charBuf[12];
-char *ts = charBuf;
-int count = 1000;
-
-static int tick = 0;
-static int tickmax = 10000;
-
-int ledState = HIGH;
-int switchState = 0;         // variable for reading the pushbutton status
-int thisPin;
-
-unsigned char lval[5],cval[5];
-
-/* This will hold the current game area */
-unsigned char area[blocks_x][blocks_y];
-
-static unsigned int timerTicks = 0;
-
-enum event_t hasEvent()
-{
-	enum event_t ret = event_none;
-
-	cval[0] = digitalRead( BUTTON_DOWN );
-	cval[1] = digitalRead( BUTTON_UP );
-	cval[2] = digitalRead( BUTTON_LEFT );
-	cval[3] = digitalRead( BUTTON_RIGHT );
-        cval[4] = digitalRead( BUTTON_RES );
-
-	if (!lval[0] && cval[0] ) {
-		ret = event_down;
-	} else if (!lval[1] && cval[1]) {
-		ret = event_up;
-	} else if (!lval[2] && cval[2]) {
-		ret = event_left;
-	} else if (!lval[3] && cval[3]) {
-		ret=  event_right;
-	} else if (cval[4] == 0) {
-		ret = event_res;
-	} 
-	*((unsigned*)lval)=*((unsigned*)cval);
-
-	return ret;
-}
-
-void getCurrentValue(char *value)
-        // subroutine for translation of value into string
-{  
-     if(val>=1000)        // for values > 999 the first digit is always 1
-       {
-         value[0]= 49;      // the first digit is 1, ASCII value 49
-         val = val - 1000;  
-        // from now on we work with the last three digits only
-       }
-       else
-       {
-         value[0]= 32;      // the first digit is a space, ASCII value 32
-       }
-       
-       int val_1;       // second digit
-       int val_2;       // third digit
-       int val_3;       // fourth digit
-       
-       val_1 = val/100;
-         // from values 100-999 only the first digit remains
-         // integers forget digits behind the decimal point
-         // f.e. 123/100 = 1,23 => 1 remains
-       val_2 = (val-(100*val_1))/10;        
-         // f.e. (123-(100*1))/10 = 2,3 => 2 remains
-       val_3 = val-(100*val_1)-(10*val_2);  
-         // f.e. 123-(100*1)-(10*2)= 3
-       value[1]= val_1+48;                  
-         // character value of 0 = 48
-         // f.e. ASCII value of 1 = 49
-       value[2]= val_2+48;             // f.e. ASCII value of 2 = 50
-     value[3]= val_3+48;             // f.e. ASCII value of 3 = 51      
-}     
-
-int read_adc(int channel){
-  int adcvalue = 0;
-  int temp;
-  byte commandbits = 00000000; //command bits - dont care (2), chn (3), dont care (3)
-
-  //allow channel selection
-  commandbits=((channel)<<3);
-    //Serial.print("CommandBits: ");
-  //Serial.print(commandbits, BIN);
-  //Serial.print(" ");
-
-  digitalWrite(SELPIN,LOW); //Select adc
-  // setup bits to be written
-  for (int i=7; i>=3; i--){
-    temp = commandbits&1<<i;
-    digitalWrite(DATAOUT,temp);
-//    Serial.print(temp, DEC);
-//    Serial.println(" ");
-    //cycle clock
-    digitalWrite(SPICLOCK,HIGH);
-    digitalWrite(SPICLOCK,LOW);
-  }
-
-
-  //read bits from adc
-  for (int i=11; i>=0; i--){
-    adcvalue+=digitalRead(DATAIN)<<i;
-    //cycle clock
-    digitalWrite(SPICLOCK,HIGH);
-    digitalWrite(SPICLOCK,LOW);
-  }
-  
-  digitalWrite(SPICLOCK,HIGH);    //ignores 2 null bits
-  digitalWrite(SPICLOCK,LOW);
-  digitalWrite(SPICLOCK,HIGH);
-  digitalWrite(SPICLOCK,LOW);
-  
-  digitalWrite(SELPIN, HIGH); //turn off device
-  return adcvalue;
-}
+static int timeout=0;
 
 void _zpu_interrupt()
 {
 	// Play
-	if (YMaudioBuffer.hasData()) {
-		int i;
-		ymframe f = YMaudioBuffer.pop();
-		for (i=0;i<16; i++) {
-			YM2149REG(i) = f.regval[i];
-		}
+	if (audioBuffer.hasData()) {
+		unsigned v = audioBuffer.pop();
+		SIGMADELTADATA = v;
 	}
-	timerTicks++;
-
+	if (timeout!=0)
+		timeout--;
 	TMR0CTL &= ~(BIT(TCTLIF));
 }
 
-void clear_area()
-{
-	memset(&area,sizeof(area),0);
-}
-
-void blitImage(const char *name)
-{
-	VGA.blitStreamInit(0, 0, VGA.getHSize());
-
-	SmallFSFile entry = SmallFS.open(name);
-	if (entry.valid()) {
-		entry.readCallback( entry.size(), &VGA.blitStream, (void*)&VGA );
-	}
-}
-
-void entryImage(const char *name)
-{
-	blitImage(name);
-	// Wait for upper key
-	event_t ev;
-	do {
-		ev = hasEvent();
-	} while (ev==event_none);
-
-}
-
-void waitTick()
-{
-#ifdef __linux__
-	sync();
-	usleep(100000);
-#endif
-	timerTicks = 0;
-	while ( timerTicks < 5 ) {
-		audiofill();
-	}
-}
-
-
-
-void processEvent( enum event_t ev )
-{
-
-        VGA.setColor(GREEN);
-
-	if (ev==event_up) {
-                VGA.printtext(10,10, "Up");
-	}
-	if (ev==event_left) {
-                VGA.printtext(10,20, "Left");
-	}
-	if (ev==event_right) {
-                VGA.printtext(10,30, "Right");
-	}
-	if (ev==event_res) {
-                //VGA.printtext(10,30, "RESET");
-                setup();
-	}
-	if (ev==event_down || tick==tickmax) {
-                VGA.printtext(10,40, "Down");
-	}
-}
-
-
-void audiofill()
-{
-	// Check audio
-	int r;
-
-#ifdef HAVE_YM
-	ymframe f;
-	while (!YMaudioBuffer.isFull()) {
-		r = ymaudiofile.read(&f.regval[0], 16);
-		if (r==0) {
-			ymaudiofile.seek(0,SEEK_SET);
-			ymaudiofile.read(&f.regval[0], 16);
-		}
-		YMaudioBuffer.push(f);
-	}
-#endif
-}
-
-void setStatus(int value, int i)
-{
-        if (value < 300)
-          VGA.setColor(GREEN);         
-        else
-          VGA.setColor(RED);
-        getCurrentValue(charBuf);
-        VGA.printtext(110,i*10, ts);          
-}
-
-void loop()
-{
-	enum event_t ev = hasEvent();
-	tick++;  
-  
-	audiofill();
-	//loop_functions[game_state]();
-	if (ev!=event_none) {
-		processEvent(ev);
-	}
-
-        //Slides
-        for (int thisPin = 0; thisPin < switchCount; thisPin++)  {
-          switchState = digitalRead(switchPins[thisPin]);
-          //switchState = HIGH;
-          digitalWrite(ledPins[thisPin], switchState);
-        }
-
-        if(tick == tickmax){
-          tick = 0;
-          for (int i = 1; i < 9; i++){
-            val = read_adc(i);
-            setStatus(val, i);
-          }       
-        }
-}
-
- 
-
 void setup()
 {
+	LS_setAllLeds(0);
+	digitalWrite(LS_ADC_SEL, 0);
+	VGAZX.begin();
+	SmallFS.begin();
 
-	pinMode(AUDIOPIN,OUTPUT);
-	digitalWrite(AUDIOPIN,HIGH);
-	outputPinForFunction(AUDIOPIN, 8);
-	pinModePPS(AUDIOPIN, HIGH);
-
-#ifdef AUDIOPINEXTRA
-	pinMode(AUDIOPINEXTRA,OUTPUT);
-	outputPinForFunction(AUDIOPINEXTRA, 8);
-	pinModePPS(AUDIOPINEXTRA, HIGH);
-#endif
-
-        //Setup Joystick
-        //pinMode(BUTTON_SEL, INPUT); 
-        pinMode(BUTTON_UP, INPUT); 
-        pinMode(BUTTON_DOWN, INPUT); 
-        pinMode(BUTTON_LEFT, INPUT); 
-        pinMode(BUTTON_RIGHT, INPUT);
-        pinMode(BUTTON_RES, INPUT); 
-       
-        //Setup SPI ADC
-        //set pin modes
-        pinMode(SELPIN, OUTPUT);
-        pinMode(DATAOUT, OUTPUT);
-        pinMode(DATAIN, INPUT);
-        pinMode(SPICLOCK, OUTPUT);
-        //disable device to start with
-        digitalWrite(SELPIN,HIGH);
-        digitalWrite(DATAOUT,LOW);
-        digitalWrite(SPICLOCK,LOW); 
-        
-        // initialize the LED pins as an output:
-        for (int thisPin = 0; thisPin < ledCount; thisPin++)  {
-          pinMode(ledPins[thisPin], OUTPUT);  
-        }
-        
-        // initialize the slides as inputs:
-        for (int thisPin = 0; thisPin < ledCount; thisPin++)  {
-          pinMode(switchPins[thisPin], INPUT);      
-        }  
-        
-//        Multiplex7Seg::set(1, 4, digitPins, segmentPins); // initialize 
-//        Multiplex7Seg::loadValue(0001); // load any int value          
-
-	VGA.clear();
+	SmallFSFile f = SmallFS.open("scr1");
+	if (f.valid())
+		VGAZX.loadscr(f);
 
 	Serial.begin(115200);
-	Serial.println("Starting");
 
-	if (SmallFS.begin()<0) {
-		Serial.println("No SmalLFS found, aborting.");
-		//while(1);
-	}
+	modfile = SmallFS.open("music.mod");
 
+	mod = pt_init_smallfs(modfile);
+	SIGMADELTACTL=0x3;
+	TMR0CTL = 0;
+	TMR0CNT = 0;
+	TMR0CMP = ((CLK_FREQ/2) / FREQ )- 1;
+	TMR0CTL = _BV(TCTLENA)|_BV(TCTLCCM)|_BV(TCTLDIR)|
+		_BV(TCTLCP0) | _BV(TCTLIEN);
 
-//	entryImage("eimage.dat");
-//	entryImage("eimage2.dat");
-
-
-	blitImage("PapilioImage.dat");
-	ymaudiofile = SmallFS.open("audio.dat");
-
-	clear_area();
-	//board_draw();
-	VGA.setColor(WHITE);
-        VGA.printtext(0,0, "Joystick");
-        VGA.printtext(0, 110, "LogicStart Test Plan");
-        VGA.printtext(100,0, "ADC");
-        VGA.printtext(80,10, "Ch0:");
-        VGA.printtext(80,20, "Ch1:");
-        VGA.printtext(80,30, "Ch2:");
-        VGA.printtext(80,40, "Ch3:");
-        VGA.printtext(80,50, "Ch4:");
-        VGA.printtext(80,60, "Ch5:");
-        VGA.printtext(80,70, "Ch6:");
-        VGA.printtext(80,80, "Ch7:");       
-        VGA.setColor(RED);
-        VGA.printtext(10,10, "Up");
-        VGA.printtext(10,20, "Left");
-        VGA.printtext(10,30, "Right");
-        VGA.printtext(10,40, "Down");
-       
 	INTRMASK = BIT(INTRLINE_TIMER0); // Enable Timer0 interrupt
+
+	USPICTL=BIT(SPICPOL)|BIT(SPISRE)|BIT(SPIEN)|BIT(SPIBLOCK)|BIT(SPICP2)|BIT(SPICP0);
 
 	INTRCTL=1;
 
-	// Start timer, 50Hz (prescaler 64)
-	TMR0CMP = (CLK_FREQ/(50U*64))-1;
-	TMR0CNT = 0x0;
-	TMR0CTL = BIT(TCTLENA)|BIT(TCTLCCM)|BIT(TCTLDIR)|BIT(TCTLCP2)|BIT(TCTLCP0)|BIT(TCTLIEN);
+
+}
+
+static unsigned char table7seg[16] =
+{
+	0x3F, //00111111, 0
+	0x06, //00000110, 1
+	0x5B, //01011011  2
+	0x4F, //01001111  3
+	0x66, //01100110  4
+	0x6D, //01101101  5
+	0x7D, //01111101  6
+	0x07, //00000111  7
+	0x7F, //01111111  8
+	0x6F, //01101111  9
+	0x00,
+	0x00,
+	0x00,
+	0x00,
+	0x00,
+	0x00
+};
+
+static int int_to_7seg(int v)
+{
+	return table7seg[v%16];
+}
+
+int cnt = 0;
+int extcnt = 0;
+int mode = 0;
+unsigned channel=0;
+
+static void set_sevenseg_value(int v, unsigned bindots)
+{
+	REGISTER( SEVENSEGBASE, 7 ) = int_to_7seg(v%10) | (bindots&8 ? 0x80:0x00);
+	REGISTER( SEVENSEGBASE, 6 ) = int_to_7seg((v/10)%10) | (bindots&4 ? 0x80:0x00);
+	REGISTER( SEVENSEGBASE, 5 ) = int_to_7seg((v/100)%10) | (bindots&2 ? 0x80:0x00);
+	REGISTER( SEVENSEGBASE, 4 ) = int_to_7seg((v/1000)%10) | (bindots&1 ? 0x80:0x00);
+}
+
+static void test()
+{
+	REGISTER( SEVENSEGBASE, 7 ) = int_to_7seg(channel);
+	REGISTER( SEVENSEGBASE, 6 ) = 0;
+	REGISTER( SEVENSEGBASE, 5 ) = SEGF|SEGE|SEGB|SEGC|SEGG;
+	REGISTER( SEVENSEGBASE, 4 ) = SEGA|SEGF|SEGE|SEGD;
+}
+
+static void hello()
+{
+	REGISTER( SEVENSEGBASE, 7 ) = SEGA|SEGF|SEGB|SEGE|SEGC|SEGD;
+	REGISTER( SEVENSEGBASE, 6 ) = SEGF|SEGE|SEGD;
+	REGISTER( SEVENSEGBASE, 5 ) = SEGA|SEGF|SEGE|SEGD|SEGG;
+	REGISTER( SEVENSEGBASE, 4 ) = SEGF|SEGE|SEGB|SEGC|SEGG;
+}
+
+static void joyTest()
+{
+	REGISTER( SEVENSEGBASE, 7 ) = 0;
+	REGISTER( SEVENSEGBASE, 6 ) = SEGF|SEGB|SEGG|SEGC|SEGD;
+	REGISTER( SEVENSEGBASE, 5 ) = SEGA|SEGB|SEGC|SEGD|SEGE|SEGF;
+	REGISTER( SEVENSEGBASE, 4 ) = SEGB|SEGC|SEGD|SEGE;
+}
+
+static void adcTest()
+{
+	REGISTER( SEVENSEGBASE, 7 ) = 0;
+	REGISTER( SEVENSEGBASE, 6 ) = SEGD|SEGE|SEGG;
+	REGISTER( SEVENSEGBASE, 5 ) = SEGB|SEGC|SEGD|SEGE|SEGG;
+	REGISTER( SEVENSEGBASE, 4 ) = SEGA|SEGB|SEGC|SEGE|SEGF|SEGG;
+}
+
+static void ledTest()
+{
+	REGISTER( SEVENSEGBASE, 7 ) = 0;
+	REGISTER( SEVENSEGBASE, 6 ) = SEGA|SEGB|SEGC|SEGD;
+	REGISTER( SEVENSEGBASE, 5 ) = SEGA|SEGD|SEGE|SEGF|SEGG;
+	REGISTER( SEVENSEGBASE, 4 ) = SEGD|SEGE|SEGF;
+}
+
+static void up()
+{
+	REGISTER( SEVENSEGBASE, 7 ) = 0;
+	REGISTER( SEVENSEGBASE, 6 ) = 0;
+	REGISTER( SEVENSEGBASE, 5 ) = SEGA|SEGB|SEGE|SEGF|SEGG;
+	REGISTER( SEVENSEGBASE, 4 ) = SEGB|SEGC|SEGD|SEGE|SEGF;
+}
+
+static void down()
+{
+	REGISTER( SEVENSEGBASE, 7 ) = SEGA|SEGB|SEGC|SEGE|SEGF;
+	REGISTER( SEVENSEGBASE, 6 ) = SEGB|SEGC|SEGD|SEGE|SEGF|SEGG;
+	REGISTER( SEVENSEGBASE, 5 ) = SEGA|SEGB|SEGC|SEGD|SEGE|SEGF;
+	REGISTER( SEVENSEGBASE, 4 ) = SEGA|SEGB|SEGC|SEGD;
+}
+
+static void left()
+{
+	REGISTER( SEVENSEGBASE, 7 ) = SEGD|SEGE|SEGF|SEGG;
+	REGISTER( SEVENSEGBASE, 6 ) = SEGA|SEGE|SEGF|SEGG;
+	REGISTER( SEVENSEGBASE, 5 ) = SEGA|SEGD|SEGE|SEGF|SEGG;
+	REGISTER( SEVENSEGBASE, 4 ) = SEGD|SEGE|SEGF;
+}
+
+static void right()
+{
+	REGISTER( SEVENSEGBASE, 7 ) = SEGD|SEGE|SEGF|SEGG;
+	REGISTER( SEVENSEGBASE, 6 ) = SEGB|SEGC|SEGE|SEGF|SEGG;
+	REGISTER( SEVENSEGBASE, 5 ) = SEGA|SEGC|SEGD|SEGE|SEGF;
+	REGISTER( SEVENSEGBASE, 4 ) = SEGA|SEGB|SEGC|SEGE|SEGG;
+}
+
+static void sel()
+{
+	REGISTER( SEVENSEGBASE, 7 ) = 0;
+	REGISTER( SEVENSEGBASE, 6 ) = SEGD|SEGE|SEGF;
+	REGISTER( SEVENSEGBASE, 5 ) = SEGA|SEGD|SEGE|SEGF|SEGG;
+	REGISTER( SEVENSEGBASE, 4 ) = SEGA|SEGC|SEGD|SEGF;
+}
+
+
+void loop()
+{
+	unsigned char lpwm;
+	unsigned switches = LS_readAllSwitches();
+	unsigned adcvalue = USPIDATA16;
+	int i;
+	const fp32_16_16 adcv = 5000;
+
+	if ((extcnt & 0x17) == 0) {
+		adcvalue &= 0xffff;
+		adcvalue <<= 4;
+		adcvalue += ((1<<4)-1);
+
+		if (adcvalue>0x10000)
+			adcvalue=0x10000;
+
+		adcvalue &= 0x1ffff;
+
+		fp32_16_16 adcf(adcvalue,1);
+		adcf *= adcv;
+
+		adcvalue = adcf.asInt();
+
+
+		//lpwm = 0xf0 - (switches& 0xf0);
+
+		LS_setAllLeds(switches);
+
+		//REGISTER( SEVENSEGBASE, 0 ) = 0xF | (lpwm<<16);
+
+          if(switches==0)
+            timeout = 17000;
+
+          //ADC Test Mode
+          if (digitalRead(LS_SWITCH0)){
+            if (timeout > 0){
+              adcTest();    
+            }  
+            else{          
+              if ((cnt & 0x3F)==0)
+  		set_sevenseg_value(adcvalue,1);              
+            }
+          }
+          
+          //LED Test Mode
+          if (digitalRead(LS_SWITCH1)){
+            if (timeout > 0){
+              ledTest();    
+            } 
+            else{           
+              set_sevenseg_value(8888,0);              
+            }
+          }  
+  
+          //LED Test Joystick         
+          if (digitalRead(LS_SWITCH2)){
+            if (timeout > 0){
+              joyTest();    
+            }
+	    if (!digitalRead(LS_JOY_SELECT))
+                sel();   
+	    if (!digitalRead(LS_JOY_DOWN))
+              down(); 
+	    if (!digitalRead(LS_JOY_RIGHT))
+              right();
+	    if (!digitalRead(LS_JOY_UP))
+              up();
+	    if (!digitalRead(LS_JOY_LEFT))
+              left();              
+          }                
+		USPIDATA16 = channel<<(8+3);
+
+
+        cnt++;
+	}
+	extcnt++;
+
+	pt_render(modfile, mod, buf, NULL, 2, 16 /* Samples */, 1, 16, 1);
+	for (i=0;i<32;i+=2) {
+		unsigned v = buf[i];
+		v += buf[i+1]<<8;
+		//v += buf[i]<<16;
+		//v += buf[i+1]<<24;
+		v+=0x8000;
+
+		while (audioBuffer.isFull());
+		audioBuffer.push(v);
+	}
 }
