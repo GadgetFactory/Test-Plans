843142b75ddd839a9bafab3bd28da652b3b4b9b9
jackgassett <jack.gassett@gadgetfactory.net>
2014-10-20 14:02:05 -0600

    Computing Shield Test Plan

--- Computing_Shield_Verification/Computing_Shield_Verification.ino
+++ Computing_Shield_Verification/Computing_Shield_Verification.ino
@@ -3,6 +3,7 @@
 #include "HQVGA.h"
 #include "SmallFS.h"
 #include "structures.h"
+#include "SPI.h"
 
 #define FREQ 17000          //Freq for ymplayer
  
@@ -13,6 +14,12 @@
 #include "cbuffer.h"
 
 MODPLAYER modplayer;
+HardwareSerial mySerial1(11);
+
+// set up variables using the SD utility library functions:
+Sd2Card card;
+SdVolume volume;
+SdFile root;
 
 #define BUTTON_DOWN 20 /* WING_B_10 */
 #define BUTTON_UP 18 /* WING_B_9 */
@@ -20,11 +27,11 @@ MODPLAYER modplayer;
 #define BUTTON_RIGHT 21 /* WING_B_11 */
 #define BUTTON_RES 19 /* Reset */
 
-//#define BUTTON_DOWN WING_B_1 /* WING_B_10 */
-//#define BUTTON_UP WING_B_3 /* WING_B_9 */
-//#define BUTTON_LEFT WING_B_4 /* WING_B_8 */
-//#define BUTTON_RIGHT WING_B_0 /* WING_B_11 */
-//#define BUTTON_RES WING_B_2 /* Reset */
+//For SD Card
+#define CSPIN  16
+#define SDIPIN 14
+#define SCKPIN 15
+#define SDOPIN 2
 
 #define PS2B_CLK 39
 #define PS2A_CLK 5
@@ -143,9 +150,9 @@ void processEvent( enum event_t ev )
 
 
 
-void setStatus(int pin, int x, int y,const char *name)
+void setStatus(int pin, int x, int y,const char *name, int highLow)
 {
-        if (digitalRead( pin ) == 1){
+        if (digitalRead( pin ) == highLow){
           VGA.setColor(GREEN);
           VGA.printtext(x,y, name);}
         else {
@@ -181,16 +188,24 @@ void loop()
     digitalWrite(LED3, ledState);
     digitalWrite(LED4, ledState); 
     
-    setStatus(PS2B_CLK,10, 70, "PS2 CLK");
-    setStatus(PS2B_DAT,10, 80, "PS2 DAT");  
+    setStatus(PS2B_CLK,10, 70, "PS2 CLK", HIGH);
+    setStatus(PS2B_DAT,10, 80, "PS2 DAT", HIGH);  
   
-    setStatus(JOYSTICKB_1, 100, 10, "J1");
-    setStatus(JOYSTICKB_2, 100, 20, "J2");
-    setStatus(JOYSTICKB_3, 100, 30, "J3");
-    setStatus(JOYSTICKB_4, 100, 40, "J4");
-    setStatus(JOYSTICKB_5, 100, 50, "J5");
-    setStatus(JOYSTICKB_6, 100, 60, "J6");
-    setStatus(JOYSTICKB_7, 100, 70, "J7");    
+    setStatus(JOYSTICKB_1, 100, 10, "J1", LOW);
+    setStatus(JOYSTICKB_2, 100, 20, "J2", LOW);
+    setStatus(JOYSTICKB_3, 100, 30, "J3", LOW);
+    setStatus(JOYSTICKB_4, 100, 40, "J4", LOW);
+    setStatus(JOYSTICKB_5, 100, 50, "J5", LOW);
+    setStatus(JOYSTICKB_6, 100, 60, "J6", LOW);
+    setStatus(JOYSTICKB_7, 100, 70, "J7", LOW); 
+ 
+    mySerial1.write("1");
+    if (mySerial1.available()) {
+     //Serial.println(mySerial1.read());
+     mySerial1.read();
+     VGA.setColor(GREEN);
+     VGA.printtext(20,90, "RS232"); 
+    }
   }      
 }
 
@@ -201,33 +216,34 @@ void setup()
   // initialize ps2 A and joystick A as HIGH outputs:
   for (int thisPin = 0; thisPin < outCount; thisPin++)  {
     pinMode(testOut[thisPin], OUTPUT); 
-    digitalWrite(testOut[thisPin],HIGH);     
+    digitalWrite(testOut[thisPin],LOW);     
   }
+  digitalWrite(PS2A_CLK,HIGH);
+  digitalWrite(PS2A_DAT,HIGH);
   
   // initialize ps2 B, joystick B and pushbutton pins as inputs:
   for (int thisPin = 0; thisPin < inCount; thisPin++)
     pinMode(testIn[thisPin], INPUT); 
   
   VGA.begin(VGAWISHBONESLOT(9),CHARMAPWISHBONESLOT(10));
-  	VGA.clear();
+  VGA.clear();
+
+  Serial.begin(115200);
+  Serial.println("Starting");
+
+  if (SmallFS.begin()<0) {
+    Serial.println("No SmalLFS found, aborting.");
+  }
+
+  blitImage("PapilioImage.dat");
   
-  	Serial.begin(115200);
-  	Serial.println("Starting");
-  
-  	if (SmallFS.begin()<0) {
-  		Serial.println("No SmalLFS found, aborting.");
-  		//while(1);
-  	}
-  
-  	blitImage("PapilioImage.dat");
-  
-  	VGA.setColor(RED);
+  VGA.setColor(RED);
   VGA.printtext(10,0, "Up");
   VGA.printtext(10,10, "Left");
   VGA.printtext(10,20, "Right");
   VGA.printtext(10,30, "Down");
   
-  VGA.printtext(0, 110, "Comp Shield Test");
+  VGA.printtext(10, 110, "Comp Shield Test");
   
   //Setup timer for YM and mod players, this generates an interrupt at 1700hz
   TMR0CTL = 0;
@@ -243,5 +259,80 @@ void setup()
   
   modplayer.loadFile("music.mod");
   modplayer.play(true);
+  
+  //delay(3000);
+  
+  VGA.printtext(20,100, "SD Card");
+  VGA.printtext(20,90, "RS232");
+  
+  //For SD Card
+  //USPICTL=BIT(SPICP1)|BIT(SPICPOL)|BIT(SPISRE)|BIT(SPIEN)|BIT(SPIBLOCK);
+//  pinMode(SDIPIN,OUTPUT);
+//  pinMode(SCKPIN,OUTPUT);
+  pinMode(CSPIN,OUTPUT); 
+ 
+  // we'll use the initialization code from the utility libraries
+  // since we're just testing if the card is working!
+  if (!card.init(SPI_FULL_SPEED, CSPIN, 12)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card is inserted?");
+    Serial.println("* Is your wiring correct?");
+    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+    return;
+  } else {
+   Serial.println("Wiring is correct and a card is present."); 
+  }  
+  
+  Serial.println("\n\nStarting Card Info:");
+  // print the type of card
+  Serial.print("Card type: ");
+  switch(card.type()) {
+    case SD_CARD_TYPE_SD1:
+      Serial.println("SD1");
+      VGA.setColor(GREEN);
+      VGA.printtext(20,100, "SD Card");
+      break;
+    case SD_CARD_TYPE_SD2:
+      Serial.println("SD2");
+      VGA.setColor(GREEN);
+      VGA.printtext(20,100, "SD Card");      
+      break;
+    case SD_CARD_TYPE_SDHC:
+      Serial.println("SDHC");
+      VGA.setColor(GREEN);
+      VGA.printtext(20,100, "SD Card");      
+      break;
+    default:
+      Serial.println("Unknown");
+  }
+
+  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+  if (!volume.init(card)) {
+    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    //return;
+  }
+
+
+  // print the type and size of the first FAT-type volume
+  uint32_t volumesize;
+  Serial.print("\nVolume type is FAT");
+  Serial.println(volume.fatType(), DEC);
+  Serial.println();
+  
+  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
+  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
+  volumesize *= 512;                            // SD card blocks are always 512 bytes
+  Serial.print("Volume size (bytes): ");
+  Serial.println(volumesize);
+  Serial.print("Volume size (Kbytes): ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+  Serial.print("Volume size (Mbytes): ");
+  volumesize /= 1024;
+  Serial.println(volumesize); 
+ 
+ //For RS232 Loopback test
+  mySerial1.begin(115200); 
+  
 
 }
